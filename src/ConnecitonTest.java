89a0924a4998145346d593b37c94bfd7bf9eb5f4
xoghkscc <82793713+xoghkscc@users.noreply.github.com>
2021-05-30 17:08:39 +0900

    21.05.30 테스트 커밋

---
+++ src/ConnecitonTest.java
@@ -0,0 +1,6 @@
+
+public class ConnecitonTest {
+	public static void main(String[] args) {
+		System.out.println("연결 테스트");
+	}
+}
--- src/GitHubTest.java
+++
@@ -1,6 +0,0 @@
-
-public class GitHubTest {
-	public static void main(String[] args) {
-		System.out.println("테스트예용");
-	}
-}
