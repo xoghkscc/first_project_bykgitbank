ae5b0acaa0243a2e479cedac7a5bc364e60c4987
xoghkscc <82793713+xoghkscc@users.noreply.github.com>
2021-06-07 11:20:52 +0900

    히카리

---
+++ src/hikariCP/HikariCP.java
@@ -0,0 +1,35 @@
+package hikariCP;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+
+public class HikariCP {
+	HikariDataSource ds;
+	public HikariCP() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl("jdbc:oracle:thin:@3.35.52.40:1521:xe");
+		config.setUsername("first_project");
+		config.setPassword("1234");
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		ds = new HikariDataSource(config);
+	}
+	
+	public HikariDataSource getHikariDataSource() {
+		return ds;
+	}
+
+		
+
+		
+
+
+}
---
+++ src/hikariCP/hikariTest.java
@@ -0,0 +1,51 @@
+package hikariCP;
+
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+
+public class hikariTest {
+	public static void main(String[] args) {
+		HikariCP cp = new HikariCP();
+		HikariDataSource ds = cp.getHikariDataSource();
+		String sql = "SELECT * FROM employees";
+		try(
+				Connection conn = ds.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery();
+		){
+			
+			while(rs.next()) {
+				System.out.print(rs.getInt(1)+"+");
+				System.out.print(rs.getString(2)+"+");
+				System.out.print(rs.getString(3));
+				System.out.println();
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
