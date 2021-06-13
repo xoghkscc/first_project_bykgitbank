e911550cb7600413e39a9bcc9f9738e2822da639
user <user@61.74.249.227>
2021-06-13 14:44:32 +0900

    기능완료

--- src/view/Members/Customer_Function/CustomerSearch.java
+++ src/view/Members/Customer_Function/CustomerSearch.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 import com.zaxxer.hikari.HikariDataSource;
 
 import function.model.Member_Informations_DB;
@@ -34,7 +36,13 @@ String sql = "SELECT * FROM member_informations WHERE members_phonenumber LIKE ?
 				String[] data = {"" + rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), 
 								"" + rs.getInt(5)};
 				MemberSearchFrame.getModel().addRow(data);
+				
+				
 			}	
+			
+			if(MemberSearchFrame.getModel().getRowCount() == 0) {
+				JOptionPane.showMessageDialog(null, "등록된회원이없습니다");
+			};
 			
 			rs.close();
 			ds.close();
